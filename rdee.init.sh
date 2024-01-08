1856ee16a89527296aec320637e0c45531193808
Roadelse <roadelse@HaSee-GX8.com>
2024-01-08 23:37:37 +0800

    inDev, rdee.init.sh

---
+++ rdee.init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+###########################################################
+# This script aims to deploy multiple rdee* repositories  #
+# on a Linux system, including WSL.                       #
+###########################################################
+
+
+# <L1> resolve arguments
+
+
+
